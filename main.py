3f33ad9fba4574f845ce9cd619fb43f167e48e9b
Rajps55 <hackinggm69@gmail.com>
2025-01-28 22:39:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-from pyrogram import Client
+from bot import Bot
 
-app = Client("my_bot")
-
-@app.on_message()
-async def handle_message(client, message):
-    await message.reply("Hello, I'm your bot!")
-
-# Running the bot
-if __name__ == "__main__":
-    app.run()  # Ensure no 'use_qr' is passed
+Bot().run()
